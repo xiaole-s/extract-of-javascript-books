64af9152335a5879d879af26d73f3b87513e302a
筱乐[xiaole] <lianle.k@163.com>
2014-02-28 20:52:40 +0800

    Update getData.js

--- ScriptedHTTP/Form/getData.js
+++ ScriptedHTTP/Form/getData.js
@@ -2,6 +2,7 @@
 function getData(url, data, callback) {
     var request = new XMLHttpRequest();
     //GET无请求体 以查询字符串形式发送数据
+    //在asp.net mfc等中还支持http://localhost:23523/parent/child1的方式
     var qUrl = url + "?" + encodeFormData(data);
     request.open("GET", qUrl);
     request.onreadystatechange = function () {
