81b44c77dce641b369bf7a620caf35ba2ce29048
筱乐[xiaole] <lianle.k@163.com>
2014-03-01 12:23:46 +0800

    Update postQueryXML.js

--- ScriptedHTTP/postQueryXML.js
+++ ScriptedHTTP/postQueryXML.js
@@ -1,4 +1,4 @@
-//XML作为主体的HTTP POST请求 
+//XML作为主体的HTTP POST请求
 function postQuery(url, what, where, radius, callback) {
     var request = new XMLHttpRequest();
     request.open("POST", url);
@@ -7,13 +7,18 @@ function postQuery(url, what, where, radius, callback) {
             callback(request);
     };
     //创建一个根元素为<query>的XML文档
+    //<query>
+    //  <find addr="where" radius="radius"> what </find>
+    //</query>
     var doc = document.implementation.createDocument("", "query", null);
     var query = doc.documentElement;
     var find = doc.createElement("find");
     query.appendChild(find);
     //设置<find>的属性
-    find.setAttribute("zipcode", where);
+    find.setAttribute("addr", where);
     find.setAttribute("radius", radius);
     //设置<find>的内容
     find.appendChild(doc.createTextNode(what));
+    //自动设置Content-Type : ext/plain; charset=UTF-8
+    request.send(doc);
 }
